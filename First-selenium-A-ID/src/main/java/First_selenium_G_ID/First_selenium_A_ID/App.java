1f893fb37b9663353f4706027c90af7bfb025b8d
sebastic <sebastic@XERAGO.ORG>
2019-05-22 11:05:23 +0530

    First-selenium-A-ID

---
+++ First-selenium-A-ID/src/main/java/First_selenium_G_ID/First_selenium_A_ID/App.java
@@ -0,0 +1,13 @@
+package First_selenium_G_ID.First_selenium_A_ID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
