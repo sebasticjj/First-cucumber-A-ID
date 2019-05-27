0e03c6134027cc8831e4af2da032ee6c7f2f0de4
sebastic <sebastic@XERAGO.ORG>
2019-05-27 12:50:53 +0530

    first commit

---
+++ First-cucumber-A-ID/src/test/java/Runner/testrunner.java
@@ -0,0 +1,15 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+ features = "Feature"
+ ,glue={"seleniumgluecode"}
+ )
+
+public class testrunner {
+
+}
